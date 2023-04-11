bdbb1dcb0a7cca9e50068efda4d9df946bfd4c99
Mayank <7silenteyes07@gmail.com>
2023-04-11 23:42:19 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -6,16 +6,14 @@ function App() {
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/The-Unleashed-Club/LinkedInRestApi"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          Project Link
         </a>
       </header>
     </div>
