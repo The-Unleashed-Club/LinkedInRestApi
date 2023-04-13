e443cdb2c6d465459c57afbf48cb37d53cb04139
naincy975 <74168443+naincy975@users.noreply.github.com>
2023-04-13 21:52:58 +0530

    Created the data of different portfolios and their GitHub links.

--- src/App.js
+++ src/App.js
@@ -1,22 +1,11 @@
-import logo from './logo.svg';
-import './App.css';
-
+import logo from "./logo.svg";
+import "./App.css";
+import Portfolio from "./pages/Portfolio";
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        
-        <a
-          className="App-link"
-          href="https://github.com/The-Unleashed-Club/LinkedInRestApi"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Project Link
-        </a>
-      </header>
-    </div>
+    <>
+      <Portfolio />
+    </>
   );
 }
 
---
+++ src/assests/data/portfolio_data.jsx
@@ -0,0 +1,99 @@
+const data = [
+  {
+    id: 1,
+    image: require("../images/img1.png"),
+    link: "https://github.com/chetanverma16/react-portfolio-template",
+  },
+  {
+    id: 2,
+    image: require("../images/img2.png"),
+    link: "https://github.com/ubaimutl/react-portfolio",
+  },
+  {
+    id: 3,
+    image: require("../images/img3.png"),
+    link: "https://github.com/shloksomani/react-portfolio",
+  },
+  {
+    id: 4,
+    image: require("../images/img4.png"),
+    link: "https://github.com/mshuber1981/github-react-portfolio-template",
+  },
+  {
+    id: 5,
+    image: require("../images/img5.png"),
+    link: "https://github.com/sunilyadav8/react-portfolio",
+  },
+  {
+    id: 6,
+    image: require("../images/img6.png"),
+    link: "https://github.com/Morganbb104/HW_20_Portfolio_withReact",
+  },
+  {
+    id: 7,
+    image: require("../images/img7.png"),
+    link: "https://github.com/rahulvijay81/portfolio?ref=reactjsexample.com",
+  },
+  ,
+  {
+    id: 8,
+    image: require("../images/img8.png"),
+    link: "https://github.com/saiful123123/React-Portfolio?ref=reactjsexample.com",
+  },
+  {
+    id: 9,
+    image: require("../images/img9.png"),
+    link: "https://github.com/soumyajit4419/Portfolio",
+  },
+  {
+    id: 10,
+    image: require("../images/img10.png"),
+    link: "https://github.com/Andy8647/react-portfolio-template?ref=reactjsexample.com",
+  },
+  {
+    id: 11,
+    image: require("../images/img11.png"),
+    link: "https://github.com/DigitalGenius-ui/portfolio-yt2?ref=reactjsexample.com",
+  },
+  {
+    id: 12,
+    image: require("../images/img12.png"),
+    link: "https://github.com/cutioluis/react-dark-portfolio?ref=reactjsexample.com",
+  },
+  {
+    id: 13,
+    image: require("../images/img13.png"),
+    link: "https://github.com/Programming-School-Pro/Awesome-Portfolio?ref=reactjsexample.com",
+  },
+  {
+    id: 14,
+    image: require("../images/img14.png"),
+    link: "https://github.com/MacieyTM/My-Website?ref=reactjsexample.com",
+  },
+  {
+    id: 15,
+    image: require("../images/img15.png"),
+    link: "https://github.com/avneesh0612/portfolio",
+  },
+  {
+    id: 16,
+    image: require("../images/img16.png"),
+    link: "https://github.com/chetanverma16/react-portfolio-template",
+  },
+  {
+    id: 17,
+    image: require("../images/img17.png"),
+    link: "https://github.com/radendi/personal-portfolio-1?ref=reactjsexample.com",
+  },
+  {
+    id: 18,
+    image: require("../images/img18.png"),
+    link: "https://github.com/adrianhajdin/project_modern_ui_ux_restaurant?ref=reactjsexample.com",
+  },
+  {
+    id: 19,
+    image: require("../images/img19.png"),
+    link: "https://github.com/chetanverma16/react-portfolio-template",
+  },
+];
+export default data;
