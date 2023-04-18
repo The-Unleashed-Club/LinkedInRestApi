0fde54d27b36e23be83e3f2cc881fefbdb84dd44
Mayank <7silenteyes07@gmail.com>
2023-04-18 18:32:12 +0530

    cc

--- src/App.js
+++ src/App.js
@@ -12,3 +12,24 @@ function App() {
 }
 
 export default App;
+
+
+// import Loginpage from "./components/Loginpage";
+// import Portfolio from "./pages/Portfolio";
+// import "./App.css";
+// import { useState } from "react";
+
+// function App() {
+//   const [show, setShow] = useState(false);
+
+//   return (
+//     <>
+//       <div className="App">
+//         {show ? <Portfolio /> : <Loginpage setShow={setShow} />}
+//       </div>
+//       ;
+//     </>
+//   );
+// }
+
+// export default App;
